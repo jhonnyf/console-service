3635db3bf98646175d4dbd63541dbae405be3c83
Jhonny Fialho <jhonny@hello-world.cc>
2021-06-29 22:56:19 -0300

    Model User

--- src/App/Models/User.php
+++ src/App/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace SenventhCode\ConsoleService\App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
