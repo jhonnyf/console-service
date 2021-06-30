a818943e56729dbbe94c1d61d7dd675f834c3947
Jhonny Fialho <jhonny@hello-world.cc>
2021-06-29 22:21:34 -0300

    Seeder + Model de user

---
+++ src/database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $response = User::create([
+            'first_name' => 'Root',
+            'last_name'  => 'Seventh',
+            'email'      => 'root@seventh.com',
+            'password'   => Hash::make('123123'),
+            'document'   => '',
+        ]);        
+    }
+}
