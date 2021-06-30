9663b6710fb3c1cebbd93cb7c9cb228eb2327996
Jhonny Fialho <jhonny@hello-world.cc>
2021-06-29 22:16:44 -0300

    migration user

---
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+   
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+
+            $table->id();
+            $table->integer('active')->default(1);
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('email')->unique();
+            $table->string('password')->nullable();
+            $table->string('document')->unique();
+            $table->string('phone', 15)->nullable();
+            $table->string('cellphone', 15)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
