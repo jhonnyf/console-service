e4eed4c744daa2f70051250ed3757a2641282444
Jhonny Fialho <jhonny@hello-world.cc>
2021-06-30 01:11:04 -0300

    routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,16 +15,20 @@ use SenventhCode\ConsoleService\App\Http\Controllers\LoginController;
 |
  */
 
-Route::group(['prefix' => 'console'], function () {
-    Route::get('login', [LoginController::class, 'index'])->name('login.index');
-    Route::get('logout', [LoginController::class, 'logout'])->name('login.logout');
-    Route::post('authenticate', [LoginController::class, 'authenticate'])->name('login.authenticate');
-});
+Route::group(['middleware' => 'web'], function () {
 
-Route::group(['prefix' => 'console'], function () {
-    Route::get('', function () {
-        return redirect()->route('dashboard');
+    Route::group(['prefix' => 'console'], function () {
+        Route::get('login', [LoginController::class, 'index'])->name('login.index');
+        Route::get('logout', [LoginController::class, 'logout'])->name('login.logout');
+        Route::post('authenticate', [LoginController::class, 'authenticate'])->name('login.authenticate');
     });
 
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::group(['middleware' => 'auth', 'prefix' => 'console'], function () {
+        Route::get('', function () {
+            return redirect()->route('dashboard');
+        });
+
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    });
+
 });
