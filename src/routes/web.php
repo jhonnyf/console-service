740cf4ab5884da2ae042b04102671cf84ebf722c
Jhonny Fialho <jhonny@hello-world.cc>
2021-08-13 23:18:04 -0300

    Novas rotas

--- src/routes/web.php
+++ src/routes/web.php
@@ -59,7 +59,7 @@ Route::group(['middleware' => 'web', 'prefix' => 'console'], function () {
 
             Route::get('export', [UserController::class, 'export'])->name('user.export');
             Route::get('download/{file_gallery_id}', [UserController::class, 'download'])->name('user.download');
-            Route::get('', [UserController::class, 'index'])->name('user.index');
+            Route::get('list/{category_id}', [UserController::class, 'index'])->name('user.index');
             Route::get('form/{id?}', [UserController::class, 'form'])->name('user.form');
             Route::post('form', [UserController::class, 'store'])->name('user.store');
             Route::put('form/{id}', [UserController::class, 'update'])->name('user.update');
@@ -78,7 +78,7 @@ Route::group(['middleware' => 'web', 'prefix' => 'console'], function () {
             Route::get('form/{id?}', [CategoryController::class, 'form'])->name('category.form');
             Route::get('active/{id}', [CategoryController::class, 'active'])->name('category.active');
             Route::get('destroy/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
-            Route::get('', [CategoryController::class, 'index'])->name('category.index');
+            Route::get('list', [CategoryController::class, 'index'])->name('category.index');
             Route::post('', [CategoryController::class, 'store'])->name('category.store');
             Route::put('{id}', [CategoryController::class, 'update'])->name('category.update');
         });
@@ -86,30 +86,12 @@ Route::group(['middleware' => 'web', 'prefix' => 'console'], function () {
         Route::group(['prefix' => 'content'], function () {
             Route::get('download/{file_gallery_id}', [ContentsController::class, 'download'])->name('content.download');
             Route::get('export', [ContentsController::class, 'export'])->name('content.export');
-            Route::get('', [ContentsController::class, 'index'])->name('content.index');
+            Route::get('list/{category_id}', [ContentsController::class, 'index'])->name('content.index');
             Route::get('form/{id?}', [ContentsController::class, 'form'])->name('content.form');
             Route::post('form', [ContentsController::class, 'store'])->name('content.store');
             Route::put('form/{id}', [ContentsController::class, 'update'])->name('content.update');
             Route::get('active/{id}', [ContentsController::class, 'active'])->name('content.active');
             Route::get('destroy/{id}', [ContentsController::class, 'destroy'])->name('content.destroy');
-        });
-
-        Route::group(['prefix' => 'language'], function () {
-            Route::get('', 'LanguagesController@index')->name('language.index');
-            Route::get('form/{id?}', 'LanguagesController@form')->name('language.form');
-            Route::post('form', 'LanguagesController@store')->name('language.store');
-            Route::put('form/{id}', 'LanguagesController@update')->name('language.update');
-            Route::get('active/{id}', 'LanguagesController@active')->name('language.active');
-            Route::get('destroy/{id}', 'LanguagesController@destroy')->name('language.destroy');
-        });
-
-        Route::group(['prefix' => 'file-gallery'], function () {
-            Route::get('', 'FilesGalleriesController@index')->name('file-gallery.index');
-            Route::get('form/{id?}', 'FilesGalleriesController@form')->name('file-gallery.form');
-            Route::post('form', 'FilesGalleriesController@store')->name('file-gallery.store');
-            Route::put('form/{id}', 'FilesGalleriesController@update')->name('file-gallery.update');
-            Route::get('active/{id}', 'FilesGalleriesController@active')->name('file-gallery.active');
-            Route::get('destroy/{id}', 'FilesGalleriesController@destroy')->name('file-gallery.destroy');
         });
 
         Route::group(['prefix' => 'file'], function () {
@@ -121,8 +103,30 @@ Route::group(['middleware' => 'web', 'prefix' => 'console'], function () {
 
             Route::post('submit/{module}/{link_id}/{file_gallery_id}', [FileController::class, 'submitFiles'])->name('file.upload-submit');
 
+            Route::get('download/{id}', [FileController::class, 'download'])->name('file.download');
             Route::get('{module}/{link_id}', [FileController::class, 'listGalleries'])->name('file.list-galleries');
+        });
 
+        /**
+         * CONFIGS
+         */
+
+        Route::group(['prefix' => 'language'], function () {
+            Route::get('list', 'LanguagesController@index')->name('language.index');
+            Route::get('form/{id?}', 'LanguagesController@form')->name('language.form');
+            Route::post('form', 'LanguagesController@store')->name('language.store');
+            Route::put('form/{id}', 'LanguagesController@update')->name('language.update');
+            Route::get('active/{id}', 'LanguagesController@active')->name('language.active');
+            Route::get('destroy/{id}', 'LanguagesController@destroy')->name('language.destroy');
+        });
+
+        Route::group(['prefix' => 'file-gallery'], function () {
+            Route::get('list', 'FilesGalleriesController@index')->name('file-gallery.index');
+            Route::get('form/{id?}', 'FilesGalleriesController@form')->name('file-gallery.form');
+            Route::post('form', 'FilesGalleriesController@store')->name('file-gallery.store');
+            Route::put('form/{id}', 'FilesGalleriesController@update')->name('file-gallery.update');
+            Route::get('active/{id}', 'FilesGalleriesController@active')->name('file-gallery.active');
+            Route::get('destroy/{id}', 'FilesGalleriesController@destroy')->name('file-gallery.destroy');
         });
     });
 });
